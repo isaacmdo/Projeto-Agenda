2b1483b68d758de3a90ebc09f3a7993a154c063c
Isaac Moura de Oliveira <isaacestudo1999@gmail.com>
2021-01-15 11:56:46 -0500

    ...

--- Agenda/routes.js
+++ Agenda/routes.js
@@ -3,16 +3,13 @@
 const express = require('express'); //<< O arquivo usa o express
 const route = express.Router(); //<< O arquivo usa a função Router do express
 const homeController = require('./src/controllers/homeController');//<<importamos o camingo do arquivo
-const contatoController = require('./src/controllers/contatoController');
-
-
-
+const loginController = require('./src/controllers/loginController');//<<importamos o camingo do arquivo
 
 //Rotas da home
-route.get('/', homeController.paginaInicial);
-route.post('/', homeController.trataPost);
+route.get('/', homeController.index);
 
-//Rotas de contato
-route.get('/contato', contatoController.paginaInicial)
+//Rotas de login
+route.get('/login/index', loginController.index);
+route.post('/login/register', loginController.register);
 
 module.exports = route;
--- Agenda/src/middlewares/middleware.js
+++ Agenda/src/middlewares/middleware.js
@@ -1,5 +1,6 @@
 exports.middlewareGlobal = (req, res, next) => {
-  res.locals.umaVariavelLocal = 'Este e o valor da variável local'
+  res.locals.errors = req.flash('errors');
+  res.locals.success = req.flash('success');
   next();
 }
 
@@ -9,9 +10,11 @@ exports.outroMiddleware = (req, res, next) => {
 };
 
 exports.checkCsrfError = (err, req, res, next) => {
-  if(err && 'EBADCSRFTOKEN' === err.code) {
-    return res.render('./includes/404');
+  if(err) {
+    return res.render('404');
   }
+
+  next();
 }
 
 exports.csrfMiddleware = (req, res, next) => {
