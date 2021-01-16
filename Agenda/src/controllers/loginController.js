8a9648a0a905ccd6a6e93b7f9d8a8b5f599a7a16
Isaac Moura de Oliveira <isaacestudo1999@gmail.com>
2021-01-16 13:14:29 -0500

    ...

--- Agenda/src/controllers/loginController.js
+++ Agenda/src/controllers/loginController.js
@@ -1,7 +1,8 @@
 const Login = require('../models/LoginModel');
 
 exports.index = (req, res) => {
-  res.render('login');
+  if(req.session.user) return res.render('login-logado');
+  return res.render('login');
 };
 
 exports.register = async function(req, res) {
@@ -56,3 +57,8 @@ exports.login = async function(req, res) {
      return res.render('404');
    }
 };
+
+exports.logout = function(req, res) {
+  req.session.destroy();
+  res.redirect('/');
+}
