b0a921d4ae6ff87cb7cdd038388dcc0b59a182d2
Isaac Moura de Oliveira <isaacestudo1999@gmail.com>
2021-01-16 11:07:34 -0500

    ...

--- Agenda/src/controllers/loginController.js
+++ Agenda/src/controllers/loginController.js
@@ -27,3 +27,32 @@ exports.register = async function(req, res) {
      return res.render('404');
    }
 };
+
+
+
+exports.login = async function(req, res) {
+  try { 
+
+    const login = new Login(req.body);
+    await login.login();
+  
+    if(login.errors.length > 0) {
+      req.flash('errors', login.errors);
+      req.session.save(function() {
+        return res.redirect('index')
+     });
+      return;
+    }
+
+    
+    req.flash('success', 'Entrando no sistema');
+    req.session.user = login.user;
+    req.session.save(function() {
+      return res.redirect('index');
+    });
+
+   } catch(e) {
+     console.log(e);
+     return res.render('404');
+   }
+};
